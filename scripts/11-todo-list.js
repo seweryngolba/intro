b77b6faafc819fdd3868f84fefcd4f98ec71eda8
Seweryn Golba <sgoolb@gmail.com>
2023-06-03 18:51:11 +0200

    exercises arrays and loops

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,5 +1,20 @@
-const todoList = [];
+const todoList = ["make dinner", "wash dishes"];
 
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+  }
+
+  console.log(todoListHTML);
+
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+}
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
   const name = inputElement.value;
@@ -8,4 +23,5 @@ function addTodo() {
   console.log(todoList);
 
   inputElement.value = "";
+  renderTodoList();
 }
